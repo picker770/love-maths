cff8e548acc3e40431af9a40453d039a9c5e2bc3
picker770 <arehman897@gmail.com>
2025-12-03 12:41:54 +0000

    set up structure of game functions in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// to run games
+
+function runGame () {}
+
+// to check user's answer
+
+function checkAnswer () {}
+
+/* to perform the calculation and return the correct answer and 
+the game type to check answer */
+
+function calculateCorrectAnswer () {}
+
+/*
+to increment the score if the answer is correct */
+
+function incrementScore() {}
+
+// to increment the score if the answer is wrong
+
+function incrementWrongAnswer() {}
+
+/* to display the questions as we are creating four questions */
+
+function displayAdditionQuestion(){}
+function displaySubtractQuestion(){}
+function displayMultiplyQuestion(){}
